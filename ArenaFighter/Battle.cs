bb86a042cd1fb48658ebcdf7a59bdfc43c4ca9d4
MrFusion3000 <nico@crepro.com>
2020-11-16 15:38:30 +0100

    Add project files.

---
+++ ArenaFighter/Battle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArenaFighter
+{
+    class Battle
+    {
+        //Fighter player = new Fighter();
+        Fighter opponent = new Fighter();
+
+        ArrayList rounds = new ArrayList();        
+
+
+    }
+}
---
+++ ArenaFighter/Dice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArenaFighter
+{
+    class Dice
+    {
+       private readonly Random _randomDice = new Random();
+
+        public int RandomDice(int min, int max)
+        {            
+            return _randomDice.Next(min,max);
+        }
+    }
+}
